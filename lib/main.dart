955a068bb2a6f7dbdc2481719e6fc4b1e14020f0
Ratie Bareeng <bareengbiz@gmail.com>
2025-02-10 14:20:02 +0200

    Add tutorial detail page

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,59 @@
+import 'package:design_patterns/data.dart';
+import 'package:design_patterns/tutorial_detail.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const TutorialApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class TutorialApp extends StatelessWidget {
+  const TutorialApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'Flutter Tutorial App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        brightness: Brightness.light,
+      ),
+      home: const TutorialHomePage(),
+    );
+  }
+}
+
+class TutorialHomePage extends StatefulWidget {
+  const TutorialHomePage({super.key});
+
+  @override
+  State<TutorialHomePage> createState() => _TutorialHomePageState();
+}
+
+class _TutorialHomePageState extends State<TutorialHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Tutorial'),
+      ),
+      body: GridView.count(
+        crossAxisCount: kIsWeb ? 4 : 2,
+        children: tutorials
+            .map((tutorial) => GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            TutorialDetailPage(tutorial: tutorial),
+                      ),
+                    );
+                  },
+                  child: Text(tutorial),
+                ))
+            .toList(),
       ),
     );
   }
---
+++ lib/tutorial_detail.dart
@@ -0,0 +1,139 @@
+import 'package:design_patterns/data.dart';
+import 'package:design_patterns/tutorial_step.dart';
+import 'package:flutter/material.dart';
+
+class TutorialDetailPage extends StatefulWidget {
+  final String tutorial;
+  const TutorialDetailPage({super.key, required this.tutorial});
+
+  @override
+  State<TutorialDetailPage> createState() => _TutorialDetailState();
+}
+
+class _TutorialDetailState extends State<TutorialDetailPage> {
+  bool isMultiPageView = false;
+  int currentPageIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Tutorial'),
+        actions: [
+          Switch(
+            value: isMultiPageView,
+            onChanged: (value) {
+              setState(() {
+                isMultiPageView = value;
+                currentPageIndex = 0;
+              });
+            },
+          ),
+          const SizedBox(width: 8),
+          Text(isMultiPageView ? 'Page View' : 'Single Page'),
+        ],
+      ),
+      body: isMultiPageView ? _buildPageView() : _buildSinglePageView(),
+    );
+  }
+
+  Widget _buildSinglePageView() {
+    return ListView.builder(
+      itemCount: tutorialSteps.length,
+      padding: const EdgeInsets.all(16),
+      itemBuilder: (context, index) {
+        return _buildTutorialStep(tutorialSteps[index]);
+      },
+    );
+  }
+
+  Widget _buildPageView() {
+    return Column(
+      children: [
+        Expanded(
+          child: PageView.builder(
+            itemCount: tutorialSteps.length,
+            onPageChanged: (index) {
+              setState(() {
+                currentPageIndex = index;
+              });
+            },
+            itemBuilder: (context, index) {
+              return SingleChildScrollView(
+                padding: const EdgeInsets.all(16),
+                child: _buildTutorialStep(tutorialSteps[index]),
+              );
+            },
+          ),
+        ),
+        _buildPageIndicator(),
+      ],
+    );
+  }
+
+  Widget _buildPageIndicator() {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          for (int i = 0; i < tutorialSteps.length; i++)
+            Container(
+              width: 8,
+              height: 8,
+              margin: const EdgeInsets.symmetric(horizontal: 4),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: i == currentPageIndex
+                    ? Theme.of(context).primaryColor
+                    : Colors.grey,
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTutorialStep(TutorialStep step) {
+    return Card(
+      margin: const EdgeInsets.only(bottom: 16),
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              step.title,
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            const SizedBox(height: 8),
+            Text(
+              step.description,
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+            const SizedBox(height: 16),
+            _buildCodeSnippet(step.codeSnippet),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCodeSnippet(String code) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.grey[900],
+        borderRadius: BorderRadius.circular(8),
+      ),
+      width: double.infinity,
+      padding: const EdgeInsets.all(16),
+      child: SelectableText(
+        code,
+        style: const TextStyle(
+          fontFamily: 'monospace',
+          color: Colors.white,
+          fontSize: 14,
+        ),
+      ),
+    );
+  }
+}
