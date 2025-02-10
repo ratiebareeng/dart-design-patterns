1b7789ab5fc44709376415e0b0bc93f65e29a7da
Ratie Bareeng <bareengbiz@gmail.com>
2025-02-10 13:49:43 +0200

    Add TutorialStep

---
+++ lib/tutorial_step.dart
@@ -0,0 +1,11 @@
+class TutorialStep {
+  final String title;
+  final String description;
+  final String codeSnippet;
+
+  TutorialStep({
+    required this.title,
+    required this.description,
+    required this.codeSnippet,
+  });
+}
